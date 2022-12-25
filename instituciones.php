9b7419e563f7f4ea9947b1e89ad58dee76a57b52
pc_oscar <twistedfoca@gmail.com>
2022-12-25 16:28:52 -0600

    obtiene instituciones y ordena en base a la distancia

--- instituciones.php
+++ instituciones.php
@@ -3,5 +3,28 @@
         require('views/instituciones.view.php');
     }else{
         require('funciones.php');
+
+        $result = ["resul"=>false];
+		$lat = $_POST['lat'];
+        $long = $_POST['long'];
+        $handler = curl_init();
+
+        $conexion = conexion();
+        $statement = $conexion->prepare('SELECT * FROM instituciones LIMIT 10');
+        $statement->execute();
+        
+        $z=1;
+        $obj="{\n";
+        while($resultado = $statement->fetch()){
+            $obj = $obj.'"'.$z.'"'.":";
+            $direccion = $resultado[4];
+            curl_setopt($handler, CURLOPT_URL, "https://maps.googleapis.com/maps/api/distancematrix/json?origins=".$lat."%2C".$long."&destinations=".$direccion."&key=AIzaSyBKg1E6WSklgCXN2Oyqm9ahBeqSZ56IaMw");
+            curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
+            $obj = $obj.curl_exec($handler).",";
+            $z++;
+        } 
+        $obj = $obj."}";
+        //echo '"'.$z.'"'.":".'""'."}";
+        echo substr_replace($obj,"",-2,-1);
     }
 ?>
--- views/js/funciones.js
+++ views/js/funciones.js
@@ -113,23 +113,39 @@ function localizacion(){
 function getdist(lat,long){
 	apdata = {lat : lat, long: long}
 	$.ajax({
-		url: "/is/pruebas.php",
+		url: "/is/instituciones.php",
 		method:'POST',
 		data: apdata,
 		success: function(data) {
 //			var result = data;
 			console.log(data);
 			var result = JSON.parse(data);
-			console.log(data);
-			if (result.resul==true) {
-				console.long("kekw");
-			}else if(result.error!=undefined){
-				for (var i = 0; i < 6; i++) {
-					if (result.error[i]!=undefined) {
-						alert(result.error[i]);
+			var i=1;
+			var distancias=[];
+			while(result[i]!=undefined){
+				distancias.push(result[i].rows[0].elements[0].distance.value);
+				console.log(result[i++]);
+			}
+			console.log(distancias);
+			distancias.sort();
+			console.log(distancias);
+			var html="",distancia1;
+			for(var j =0; j<3;j++){
+				console.log("while grande\n");
+				i = 1;
+				while(i<=3){
+					distancia1=result[i].rows[0].elements[0].distance.value;
+					if(distancias[j]==distancia1){
+						//datos html será la estructura html para mostrar las instituciones, deberá cambiar dinámicamente
+						html += "datos html "+distancia1+"\n";
+						i=4;
+					}else{
+						i++;
 					}
 				}
 			}
+			console.log(html);
+			//console.log(result["1"].rows[0].elements[0].distance.text);
 		}
 	});
 }
