f680c85fe0d244888890fb62bbfc8c1a1c00af5e
lap_oscar <twistedfoca@gmail.com>
2023-01-03 23:46:29 -0600

    pruebas

--- pruebas.php
+++ pruebas.php
@@ -2,16 +2,6 @@
     if ($_SERVER['REQUEST_METHOD']=='GET'){
         require('views/pruebas.view.php');
     }else{
-        require('funciones.php');
-
-        $result = ["resul"=>false];
-		$lat = $_POST['lat'];
-        $long = $_POST['long'];
-        $handler = curl_init();
-
-        curl_setopt($handler, CURLOPT_URL, "https://maps.googleapis.com/maps/api/distancematrix/json?origins=".$lat."%2C".$long."&destinations=Mexico+57500&key=AIzaSyBKg1E6WSklgCXN2Oyqm9ahBeqSZ56IaMw");
-        curl_setopt($handler, CURLOPT_RETURNTRANSFER, false);
-        curl_exec($handler);  
-        
+        echo $_POST["data"];
     }
 ?>
