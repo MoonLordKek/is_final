289d6a1480b8f471f9a33aa88999384d26effb30
Fernando1243 <120618566+Fernando1243@users.noreply.github.com>
2023-01-06 22:42:22 -0600

    Update iniciar_sesion.view.php

--- views/iniciar_sesion.view.php
+++ views/iniciar_sesion.view.php
@@ -1,75 +1,85 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
+ 
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=3.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" >
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagina Principal</title>
-    
+    <title>Bienvenido</title>
     <!-- tipografia -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto:wght@500&display=swap" rel="stylesheet">
-
+    
     <!-- iconos -->
     <script src="https://kit.fontawesome.com/69e2fb8eaa.js" crossorigin="anonymous"></script>
-
+    
     <!-- hoja de estilos -->
-    <link rel="stylesheet" href="./views/build/css/app.css">
-    <!-- jqery & ajax -->
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-</head>
-
+    <link rel="stylesheet" href="./build/css/app.css">
+    <link rel="stylesheet" href="./build/css/estilos.css">
+	
+</head>  
 <body>
-
     <header>
         <div class="contenedormenu contenedor">
             <div class="logo">
-                <img src="./views/src/img/logot.png" alt="logo">
+                <img src="./src/img/logot.png" alt="logo">
             </div>
             <div class="menu">
                 <i class="fa-solid fa-bars" id="btn-menu"></i>
                 <div id="backmenu"></div>
                 <nav id="nav" class="nav">
-                    <img src="./views/src/img/logot.png" alt="logo">
+                    <img src="./src/img/logot.png" alt="logo">
                     <ul>
-                        <li class="ubic"><a href="#">Inicio</a></li>
-                        <li><a href="./cuestionario.php">Tests</a></li>
-                        <li><a href="./instituciones.php">Instituciones</a></li>
-                        <li><a href="./yomecuido.php">Tecnicas</a></li>
-                        <li><a href="./evaluacion.php">Evaluación</a></li>
+                        <li><a href="./index.html">Inicio</a></li>
+                        <li><a href="#">Tests</a></li>
+                        <li><a href="./instituciones.html">Instituciones</a></li>
+                        <li><a href="./yomecuido.html">Tecnicas</a></li>
+                        <li><a href="./evaluacion.html">Evaluación</a></li>
+                        <li class="ubic"><a href="./iniciar-sesion.html">Log in</a></li>
+                        <li><a href="./registro.html">Sign up</a></li>
                     </ul>
                 </nav>
             </div>
         </div>
     </header>
-
-    <div class="tabla contenedor">
-        <div class="tabla">
-            <input type="text" id="email" placeholder="correo electrónico">
-        </div>
-        <div class="tabla" >
-            <input type="password" id="pass" placeholder="contraseña">    
-        </div>
-        <div class="tabla">
-            <button class="button" onclick="iniciar_Ses()">Iniciar sesion</button>
-        </div>        
-    </div>
+    <form class="formulario">
     
-
+        <h1>Bienvenido de vuelta</h1>
+         <div class="contenedor">
+         
+         
+             
+             <div class="input-contenedor">
+             <i class="fas fa-envelope icon"></i>
+             <input type="text" placeholder="Correo Electronico">
+             
+             </div>
+             
+             <div class="input-contenedor">
+            <i class="fas fa-key icon"></i>
+             <input type="password" placeholder="Contraseña">
+             
+             </div>
+             <input type="submit" value="Login" class="button">
+             <p>Al registrarte, aceptas nuestras Condiciones de uso y Política de privacidad.</p>
+             <p>¿No tienes una cuenta? <a class="link" href="./registro.html">Registrate </a></p>
+             <p><a class="link" href="#">¿Olvidaste la contraseña?</a></p>
+         </div>
+        </form>
     <footer class="contenedor">
-
         <div class="logo">
-            <img src="./views/src/img/logot.png" alt="logo" style="width: 3rem;">
+            <img src="./src/img/logot.png" alt="logo" style="width: 3rem;">
         </div>
         <div class="about">
             <a href="#">About</a>
         </div>
     </footer>
-
-    <!-- js animaciones -->
-    <script src="./views/src/js/gsap.js"></script>
-    <!-- mi js -->
-    <script src="./views/js/funciones.js"></script>
+        <!-- js animaciones -->
+        <script src="./src/js/gsap.js"></script>
+        <!-- mi js -->
+        <script src="./src/js/app.js"></script>
 </body>
 </html>
